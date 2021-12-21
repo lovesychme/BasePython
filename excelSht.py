b5573999c29782bc0cb5cac25055e57a01c7d248
P1340814 <Pass@2021-1>
2021-12-21 16:56:04 +0800

    config

--- excelSht.py
+++ excelSht.py
@@ -1,13 +1,16 @@
 from  excelUtil import *
-from xlConst import *
+from  xlConst import *
 
 class ExcelSht(ExcelUtil):
+    KEY_BREAK='_@_'
+    SHT_NAME='UnTitle'
     def __init__(self):
         self.titleRow=None
         self.headRow=None
         self.sht=None
         self.headColDic=None
-
+        self.primaryKeys:[]=None   #行数据的关键字段
+        self.heads:[]=None
     @property
     def maxRow(self):
         return self.sht.UsedRange.Rows.Count - self.sht.UsedRange.Row + 1
@@ -16,7 +19,7 @@ class ExcelSht(ExcelUtil):
         return self.sht.UsedRange.Columns.Count - self.sht.UsedRange.Column + 1
     def getCol(self,head:str):
         return self.headColDic[head]
-    def initExcelSht(self, sht):
+    def initSht(self, sht):
         self.sht = sht
         dic={}
         for i in range(1,self.maxCol+1):
@@ -24,6 +27,14 @@ class ExcelSht(ExcelUtil):
             if val:
                 dic[val]=i
         self.headColDic=dic
+
+        if self.primaryKeys:
+            keyCols=[self.headColDic[item] for item in self.primaryKeys]
+            for iRow in range(self.headRow+1, self.maxRow):
+                key=self.KEY_BREAK.join([sht.Cells(iRow,iCol).Value for iCol in keyCols])
+                self.keyRowDic[key]=iRow
+
+
     def get_all_data(self):
         sht=self.sht
         data=[]
@@ -36,11 +47,6 @@ class ExcelSht(ExcelUtil):
             data.append(dic)
         return data
 
-    def addData(self,dic:dict):
-        row=self.maxRow+1
-        for k,v in dic.items():
-            if k in self.headColDic:
-                self.sht.Cells(row,self.headColDic[k]).Value=v
     def deleteRow(self,rowNumber):
         self.sht.Rows(rowNumber).Delete(Shift=xlUp)
     def deleteRngXlUp(self,rng):
@@ -48,3 +54,53 @@ class ExcelSht(ExcelUtil):
     def setValueColor(self,rng,value,color=None):
         rng.Value=value
         rng.Interior.Color=color
+
+    def writeTitle(self, rng, title, fontSize=12, fontBold=True):
+        #设置Title格式
+        self.setBorders(rng)
+        rng.Merge()
+        self.setAlignCenter(rng)
+        rng.Font.Size = fontSize
+        rng.Font.Bold = fontBold
+        rng.Value=title
+
+    def writeHeads(self,heads):
+        sht=self.sht
+        for i,s in enumerate(heads):
+            sht.Cells(self.headRow,i+1).Value=s
+            sht.Columns(i+1).EntireColumn.AutoFit()
+        maxCol=i+1
+
+        rng=sht.Range(sht.Cells(self.headRow,1),sht.Cells(self.headRow,maxCol))
+        self.setAlignCenter(rng)
+        self.setBackColor(rng,xlColor.GRAY) #灰色
+        self.backColor=xlColor.GRAY
+        self.setBorders(rng)
+
+    def new(self, sht):
+        sht.Name = self.SHT_NAME
+        self.sht = sht
+        # 填写Heads
+        self.writeHeads(self.heads)
+
+        # 填写Title
+        rng = sht.Range(sht.Cells(self.titleRow, 1), sht.Cells(self.titleRow, self.maxCol))
+        self.writeTitle(rng, sht.Name)
+        self.initSht(sht)
+
+    def getItem(self,keys,item):
+        key=self.KEY_BREAK.join(keys)
+        if key in self.keyRowDic:
+            return self.sht.Cells(self.keyRowDic[key],self.headColDic[item]).Value
+
+    def setKeyItem(self,keys,item,value):
+        key = self.KEY_BREAK.join(keys)
+        if key not in self.keyRowDic:
+            self.addKey(key)
+        self.sht.Cells(self.keyRowDic[key], self.headColDic[item]).Value = value
+
+    def append(self, dic:dict):
+        row=self.maxRow+1
+        for k,v in dic.items():
+            if k in self.headColDic:
+                self.sht.Cells(row,self.headColDic[k]).Value=v
